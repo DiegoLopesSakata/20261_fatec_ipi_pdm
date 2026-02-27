36d811b608669865b315183334ee184691c21b15
Diego Lopes Sakata <dihsakata@gmail.com>
2026-02-27 18:21:32 -0300

    learn: encerra codigo 2.5.4
    -vimos o grid system do bootstrap
    - a solucao esta adaptada para lidar com quatro pedidos
    - o mecanismo de responsividade foi atualizado

--- ap04-props/src/App.jsx
+++ ap04-props/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => (
+    <div className="container border mt-2">
+        <div className="row">
+            <div className="col-12">
+                {/* i.fa.fa-hippo */}
+                <div className="fa fa-hippo fa-2x p-3"></div>
+            </div>
+        </div>
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        <div className="row">
+            <div className="col-sm-12 col-lg-6 col-xxl-3"> 
+                <div className="py-2 border">Pedido 1</div>
+            </div>   
+            <div className="col-sm-12 col-lg-6 col-xxl-3">
+                <div className="py-2 border">Pedido 2</div>
+            </div>   
+            <div className="col-sm-12 col-lg-6 col-xxl-3">
+                <div className="py-2 border">Pedido 3</div>
+            </div>   
+            <div className="col-sm-12 col-lg-6 col-xxl-3">
+                <div className="py-2 border">Pedido 4</div>
+            </div>
+        </div>
+    </div>
+)
 
 export default App
---
+++ ap04-props/src/old_App.jsx
@@ -0,0 +1,42 @@
+const app = () => (
+  <div className="container border py-5">
+    <div className="row">
+      <div className="col-sm-12 col-md-3 col-lg-3">
+        <div className="p-4 bg-primary">
+
+        </div>
+      </div>
+      <div className="col-sm-12 col-md-9 col-lg-9">
+        <div className="p-4 bg-secondary">
+
+        </div>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col-sm-12 col-md-6 col-lg-4">
+        <div className="p-4 bg-info border">
+
+        </div>
+      </div>
+      <div className="col-sm-12 col-md-6 col-lg-4">
+        <div className="p-4 bg-info border">
+
+        </div>
+      </div>
+      <div className="col-sm-12 col-md-12 col-lg-4">
+        <div className="p-4 bg-info border">
+
+        </div>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col-12">
+        <div className="p-4 bg-success"></div>
+      </div>
+    </div>
+  </div>
+)
+
+export default app
