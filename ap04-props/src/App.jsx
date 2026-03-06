9f25d59cf38cd72e7f193d6852f4eb325c054cd5
Diego Lopes Sakata <dihsakata@gmail.com>
2026-03-06 16:13:31 -0300

    learn: encerra apostila 2.5

--- ap04-props/src/App.jsx
+++ ap04-props/src/App.jsx
@@ -9,16 +9,68 @@ const App = () => (
 
         <div className="row">
             <div className="col-sm-12 col-lg-6 col-xxl-3"> 
-                <div className="py-2 border">Pedido 1</div>
+                <div className="py-2 border">
+                    <div className="card">
+                        <div className="card-header text-muted">15/04/2025</div>
+                        <div className="card-body d-flex">
+                            <div className="d-flex align-items-center">
+                                <i className="fa-solid fa-bowling-ball fa-2x"></i>
+                            </div>
+                            <div className="ms-2 flex-grow-1 border rounded p-2">
+                                <h4 className="text-center">Bola de boliche</h4>
+                                <p className="text-center">Uma bola de boliche</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>   
             <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="py-2 border">Pedido 2</div>
+                <div className="py-2 border">
+                    <div className="card">
+                        <div className="card-header text-muted">15/04/2025</div>
+                        <div className="card-body d-flex">
+                            <div className="d-flex align-items-center">
+                                <i className="fa-solid fa-bowling-ball fa-2x"></i>
+                            </div>
+                            <div className="ms-2 flex-grow-1 border rounded p-2">
+                                <h4 className="text-center">Bola de boliche</h4>
+                                <p className="text-center">Uma bola de boliche</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>   
             <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="py-2 border">Pedido 3</div>
+                <div className="py-2 border">
+                    <div className="card">
+                        <div className="card-header text-muted">15/04/2025</div>
+                        <div className="card-body d-flex">
+                            <div className="d-flex align-items-center">
+                                <i className="fa-solid fa-bowling-ball fa-2x"></i>
+                            </div>
+                            <div className="ms-2 flex-grow-1 border rounded p-2">
+                                <h4 className="text-center">Bola de boliche</h4>
+                                <p className="text-center">Uma bola de boliche</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>   
             <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="py-2 border">Pedido 4</div>
+                <div className="py-2 border">
+                    <div className="card">
+                        <div className="card-header text-muted">15/04/2025</div>
+                        <div className="card-body d-flex">
+                            <div className="d-flex align-items-center">
+                                <i className="fa-solid fa-bowling-ball fa-2x"></i>
+                            </div>
+                            <div className="ms-2 flex-grow-1 border rounded p-2">
+                                <h4 className="text-center">Bola de boliche</h4>
+                                <p className="text-center">Uma bola de boliche</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
